492f1c4a2a24e79bd78493e2a74c933fdeedd745
bgoodson <bgoodson@kilo.local>
2021-10-30 21:06:25 -0400

    add source

---
+++ src/index.js
@@ -0,0 +1,58 @@
+const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
+const multer = require('multer')
+const cookieParser = require('cookie-parser')
+const { version } = require('../package.json')
+const crypto = require('crypto')
+
+// helpers functions
+const randomString = function (len) {
+  var buf = []
+    , chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+    , charlen = chars.length;
+
+  for (var i = 0; i < len; ++i) {
+    buf.push(chars[getRandomInt(0, charlen - 1)]);
+  }
+
+  return buf.join('');
+};
+
+const getKey = () => new Promise((resolve, reject) => {
+  const key = crypto.randomBytes(48, (error, buffer) => {
+    const key = buffer.toString('hex')
+    resolve(key)
+  })
+})
+
+// instantiate web server
+const storage = multer.memoryStorage()
+const upload = multer({ storage, preservePath: true })
+const app = express()
+const port = process.env.PORT || 5000
+const PROTOCOL = process.env.PROTOCOL || 'http'
+const HOST = process.env.HOST || 'localhost'
+
+// configure api server
+app.use(cookieParser())
+app.use(express.urlencoded({ extended: true }))
+app.use(cors({ credentials: true, origin: `${PROTOCOL}://${HOST}:${port}` }))
+app.use(morgan('tiny'))
+app.use((req, res, next) => {
+  res.set('X-Blackwater-Creek-API-Version', `v${version}`)
+  next()
+})
+
+// api routes
+app.get('/', async (req, res) => {
+  const key = await getKey()
+  res.send({
+    message: key,
+    version
+  })
+  res.end()
+})
+
+// web app serverp
+app.listen(port, () => console.log(`Blackwater Creek API listening at ${PROTOCOL}://${HOST}:${port}`))
